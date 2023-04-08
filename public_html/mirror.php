1373c2bd6075fb2bd5ac5f53f0e91756979b0e18
diznq <13458283+diznq@users.noreply.github.com>
2023-04-08 17:00:26 +0200

    feat: old client binaries

---
+++ public_html/mirror.php
@@ -0,0 +1,11 @@
+https://crymp.net/client/v11/dl/Mods/sfwcl/Bin32/sfwcl.dll
+https://crymp.net/client/v11/dl/Mods/sfwcl/Bin32/mapdl.dll
+https://crymp.net/client/v11/dl/Mods/sfwcl/Bin64/sfwcl.dll
+https://crymp.net/client/v11/dl/Mods/sfwcl/Bin64/mapdl.dll
+https://crymp.net/client/v11/dl/Mods/sfwcl/Files/IntegrityService.bin
+https://crymp.net/client/v11/dl/Mods/sfwcl/Files/GameRules.bin
+https://crymp.net/client/v11/dl/Mods/sfwcl/Files/Main.bin
+https://crymp.net/client/v11/dl/Mods/sfwcl/Game/Scripts/Entities/Items/XML/Weapons/Multiplayer/LAW.xml
+https://crymp.net/client/v11/dl/Mods/sfwcl/Game/Scripts/Entities/Items/XML/OffHand.xml
+https://crymp.net/client/v11/dl/Bin32/game.exe
+https://crymp.net/client/v11/dl/Bin64/game.exe<?lu header("content-type", "text/plain") ?>

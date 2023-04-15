3f7f446cfcb587f9db8c6af3ee999282d3e7be2e
diznq <13458283+diznq@users.noreply.github.com>
2023-04-15 12:50:09 +0200

    fix: change biases

--- web_api.lua
+++ web_api.lua
@@ -285,7 +285,7 @@ function api:upsertServer(params)
                 params.peopleTime = existingServer.peopleTime + params.numPlayers * timeDelta
                 params.rating = existingServer.rating or 0
                 if params.numPlayers <= 0 then
-                    params.rating = existingServer.rating / math.max(1.0001, (1 + 0.0005 * ratio))
+                    params.rating = existingServer.rating / math.max(1.0001, (1 + 0.0007 * ratio))
                 else
                     params.rating = existingServer.rating + (math.sqrt(params.numPlayers) * 0.0005 * ratio)
                 end

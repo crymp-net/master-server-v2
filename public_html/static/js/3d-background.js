e0587d8587ae9816aa4a5b3ee3179b413840e88f
diznq <13458283+diznq@users.noreply.github.com>
2023-10-26 18:49:02 +0200

    feat: add new backgrounds

--- public_html/static/js/3d-background.js
+++ public_html/static/js/3d-background.js
@@ -9,38 +9,82 @@ function init3DBackground(){
     const fgPath = "/static/images/3D_" + imgId + "FG.jpg";
     const depthPath = "/static/images/3D_" + imgId + "DM.jpg";
 
-    const stage = new PIXI.Container();
-    const foreground = new PIXI.Container();
     const loader = new PIXI.loaders.Loader();
-    stage.addChild(foreground); 
     output.appendChild(renderer.view);
 
+    renderer.view.style.width = "100vw";
+    renderer.view.style.height = "100vh";
+
     let mouseX = w/2, mouseY = h/2;
-    loader.add("fg",     fgPath)
+    loader.add("diffuse",   fgPath)
     loader.add("depth",  depthPath)
     loader.once("complete", () => {
-        const fg = new PIXI.Sprite(loader.resources.fg.texture);
-        const depth = new PIXI.Sprite(loader.resources.depth.texture)
-        const filter = new PIXI.filters.DisplacementFilter(depth, 0);
+        const stage = new PIXI.Container();
+        const box = new PIXI.Graphics();
+
+        const diffuse = loader.resources.diffuse;
+        const depth = loader.resources.depth;
+
+        const filter = new PIXI.Filter(null, `
+        precision mediump float;
+
+        varying vec2 vTextureCoord;
+        
+        uniform sampler2D uSampler;
+        uniform sampler2D diffuse;
+        uniform sampler2D depth;
+        
+        uniform vec2 resolution;
+        uniform vec2 cursor;
+        uniform float ar;
+
+        #define AR (16.0 / 9.0)
+
+        void main(){
+            vec2 pixel = 1.0 / resolution;
+            vec2 disp = (cursor - 0.5) * 2.0 * pixel;
+            vec2 uv = vec2(vTextureCoord.x, vTextureCoord.y);
+            vec2 bias = vec2(1.0, 1.0);
+            if(ar > AR){
+                bias.y = AR / ar;
+                uv.y += 1.0 - AR / ar;
+            } else if(ar < AR) {
+                bias.x = ar / AR;
+                uv.x += 1.0 - ar / AR;
+            }
+            uv = uv * bias;
+            disp = disp * bias;
+            float vDepth = 1.0 - texture2D(depth, uv).r;
+            vec2 distortedUv = uv - vDepth * disp * 7.0;
+            vec3 vnColor = texture2D(diffuse, distortedUv).rgb;
+            gl_FragColor = vec4(vnColor, 1.0);
+        }`)
+
+
+        filter.uniforms.diffuse = diffuse.texture;
+        filter.uniforms.depth = depth.texture;
 
         function updateStage() {
-            renderer.resize(window.innerWidth, window.innerHeight)
-            stage.scale.x = Math.max(window.innerWidth / fg.width, window.innerHeight / fg.height)
-            stage.scale.y = stage.scale.x;
+            renderer.resize(diffuse.texture.width, diffuse.texture.height)
+            filter.uniforms.resolution = [diffuse.texture.width, diffuse.texture.height];
+            filter.uniforms.ar = window.innerWidth/(window.innerHeight - 100);
+            
+            box.beginFill(0x000000);
+            box.drawRect(0, 0, diffuse.texture.width, diffuse.texture.height);
+            box.endFill();
         }
 
+        box.filters = [filter];
 
+        stage.addChild(box);
+        
         updateStage();
 
-        depth.renderable = false;
-        fg.addChild(depth);
-        foreground.addChild(fg);    
-        
-        fg.filters = [filter];
-
         function animate() {
-            filter.scale.x = 20 * (window.innerWidth/2 - mouseX) / window.innerWidth;
-            filter.scale.y = 20 * (window.innerHeight/2 - mouseY) / window.innerHeight;
+            filter.uniforms.cursor = [
+                mouseX / window.innerWidth,
+                mouseY / window.innerHeight
+            ];
             renderer.render(stage);       
         }
 

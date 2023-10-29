ef3311cab9c4ebd51dc0e5d1a8149ba8811d1af0
diznq <13458283+diznq@users.noreply.github.com>
2023-10-29 12:29:35 +0100

    chore: more images

--- public_html/static/js/3d-background.js
+++ public_html/static/js/3d-background.js
@@ -1,3 +1,88 @@
+const VIDEO_SHADER = `
+precision mediump float;
+
+varying vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
+uniform sampler2D diffuse;
+
+uniform vec2 resolution;
+uniform vec2 cursor;
+uniform float ar;
+
+#define AR (16.0 / 9.0)
+
+int extract_16(int filtered) {
+    if(filtered >= 128) filtered -= 128;
+    if(filtered >= 64) filtered -= 64;
+    if(filtered >= 32) filtered -= 32;
+    if(filtered >= 16) filtered -= 16;
+    return filtered;
+}
+
+void main(){
+    vec2 pixel = 1.0 / resolution;
+    vec2 disp = (cursor - 0.5) * 2.0 * pixel;
+    vec2 uv = vec2(vTextureCoord.x, vTextureCoord.y);
+    vec2 bias = vec2(1.0, 1.0);
+    if(ar > AR){
+        bias.y = AR / ar;
+        uv.y += 1.0 - AR / ar;
+    } else if(ar < AR) {
+        bias.x = ar / AR;
+        uv.x += 1.0 - ar / AR;
+    }
+    uv = uv * bias;
+    disp = disp * bias;
+    float b_chan = texture2D(diffuse, uv).b;
+    int ib_chan = int(255.0 * b_chan);
+    int depth_bits = extract_16(ib_chan);
+
+    float vDepth = 1.0 - float(depth_bits) / 15.0;
+    vec2 distortedUv = uv - vDepth * disp * 7.0;
+    vec3 vnColor = texture2D(diffuse, distortedUv).rgb;
+    int nb_chan = int(vnColor.b * 255.0);
+    depth_bits = extract_16(ib_chan);
+    nb_chan -= depth_bits;
+    vnColor.b = float(nb_chan) / 255.0;
+    gl_FragColor = vec4(vnColor, 1.0);
+}`;
+
+const PICTURE_SHADER = `
+precision mediump float;
+
+varying vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
+uniform sampler2D diffuse;
+uniform sampler2D depth;
+
+uniform vec2 resolution;
+uniform vec2 cursor;
+uniform float ar;
+
+#define AR (16.0 / 9.0)
+
+void main(){
+    vec2 pixel = 1.0 / resolution;
+    vec2 disp = (cursor - 0.5) * 2.0 * pixel;
+    vec2 uv = vec2(vTextureCoord.x, vTextureCoord.y);
+    vec2 bias = vec2(1.0, 1.0);
+    if(ar > AR){
+        bias.y = AR / ar;
+        uv.y += 1.0 - AR / ar;
+    } else if(ar < AR) {
+        bias.x = ar / AR;
+        uv.x += 1.0 - ar / AR;
+    }
+    uv = uv * bias;
+    disp = disp * bias;
+    float vDepth = 1.0 - texture2D(depth, uv).r;
+    vec2 distortedUv = uv - vDepth * disp * 7.0;
+    vec3 vnColor = texture2D(diffuse, distortedUv).rgb;
+    gl_FragColor = vec4(vnColor, 1.0);
+}`;
+
 function init3DBackground(){
     let w = window.innerWidth;
     let h = window.innerHeight;
@@ -5,9 +90,11 @@ function init3DBackground(){
     const renderer = new PIXI.WebGLRenderer(w, h);
     const output = document.getElementById("wrap");
 
-    const imgId = output.getAttribute("data-image")
+    const imgId = output.getAttribute("data-image");
     const fgPath = "/static/images/round_2/3D_FG" + imgId + ".jpg";
     const depthPath = "/static/images/round_2/3D_DM" + imgId + ".jpg";
+
+    const isVideo = fgPath.indexOf(".mp4") != -1;
 
     const loader = new PIXI.loaders.Loader();
     output.appendChild(renderer.view);
@@ -16,61 +103,49 @@ function init3DBackground(){
     renderer.view.style.height = "100vh";
 
     let mouseX = w/2, mouseY = h/2;
-    loader.add("diffuse",   fgPath)
-    loader.add("depth",  depthPath)
+    loader.add("diffuse", fgPath)
+    if(!isVideo) {
+        loader.add("depth",  depthPath)
+    }
     loader.once("complete", () => {
         const stage = new PIXI.Container();
         const box = new PIXI.Graphics();
 
-        const diffuse = loader.resources.diffuse;
+        let diffuse = null;
+        let dWidth = 0, dHeight = 0;
+
+        if(isVideo) {
+            const video = loader.resources.diffuse.data;
+            video.muted = true;
+            video.loop = true;
+            
+            diffuse = new PIXI.Texture.fromVideo(video);
+            dWidth = diffuse.width;
+            dHeight = diffuse.height;
+        } else {
+            diffuse = loader.resources.diffuse;
+            dWidth = diffuse.texture.width;
+            dHeight = diffuse.texture.height;
+        }
         const depth = loader.resources.depth;
 
-        const filter = new PIXI.Filter(null, `
-        precision mediump float;
 
-        varying vec2 vTextureCoord;
-        
-        uniform sampler2D uSampler;
-        uniform sampler2D diffuse;
-        uniform sampler2D depth;
-        
-        uniform vec2 resolution;
-        uniform vec2 cursor;
-        uniform float ar;
+        const filter = new PIXI.Filter(null, isVideo ? VIDEO_SHADER : PICTURE_SHADER)
 
-        #define AR (16.0 / 9.0)
-
-        void main(){
-            vec2 pixel = 1.0 / resolution;
-            vec2 disp = (cursor - 0.5) * 2.0 * pixel;
-            vec2 uv = vec2(vTextureCoord.x, vTextureCoord.y);
-            vec2 bias = vec2(1.0, 1.0);
-            if(ar > AR){
-                bias.y = AR / ar;
-                uv.y += 1.0 - AR / ar;
-            } else if(ar < AR) {
-                bias.x = ar / AR;
-                uv.x += 1.0 - ar / AR;
-            }
-            uv = uv * bias;
-            disp = disp * bias;
-            float vDepth = 1.0 - texture2D(depth, uv).r;
-            vec2 distortedUv = uv - vDepth * disp * 7.0;
-            vec3 vnColor = texture2D(diffuse, distortedUv).rgb;
-            gl_FragColor = vec4(vnColor, 1.0);
-        }`)
-
-
-        filter.uniforms.diffuse = diffuse.texture;
-        filter.uniforms.depth = depth.texture;
+        if(isVideo) {
+            filter.uniforms.diffuse = diffuse;
+        } else {
+            filter.uniforms.diffuse = diffuse.texture;
+            filter.uniforms.depth = depth.texture;
+        }
 
         function updateStage() {
-            renderer.resize(diffuse.texture.width, diffuse.texture.height)
-            filter.uniforms.resolution = [diffuse.texture.width, diffuse.texture.height];
+            renderer.resize(dWidth, dHeight)
+            filter.uniforms.resolution = [dWidth, dHeight];
             filter.uniforms.ar = window.innerWidth/(window.innerHeight - 100);
             
             box.beginFill(0x000000);
-            box.drawRect(0, 0, diffuse.texture.width, diffuse.texture.height);
+            box.drawRect(0, 0, dWidth, dHeight);
             box.endFill();
         }
 
